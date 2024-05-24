29c12a3dc474973ef656cd894535a8dab7e9854c
dexterski007 <mbouj007@gmail.com>
2024-05-24 13:30:56 -0700

    indent

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -21,12 +21,12 @@ def status():
 @app_views.route('/stats')
 def stats():
     stats = {
-    "amenities": storage.count(Amenity),
-    "cities": storage.count(City),
-    "places": storage.count(Place),
-    "reviews": storage.count(Review),
-    "states": storage.count(State),
-    "users": storage.count(User)
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
     }
 
     return jsonify(stats)
