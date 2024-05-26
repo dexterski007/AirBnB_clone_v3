330114c6c46bb483d4d9c7fb6ff07a864993b72e
dexterski007 <mbouj007@gmail.com>
2024-05-26 06:37:48 -0700

    fix

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -50,7 +50,6 @@ def states_p():
     if "name" not in state_imp:
         abort(400, "Missing name")
     state_new = State(**state_imp)
-    storage.new(state_new)
     storage.save()
     return jsonify(state_new.to_dict()), 201
 
